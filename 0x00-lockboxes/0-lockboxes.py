a776673ef35a2e397e25e8988b6d66b9c0062e8c
Bre <49533377+brerickner@users.noreply.github.com>
2021-07-21 11:06:39 -0500

    pep8 compliant

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-'''This Module counts determines if there are adequate keys to open n-1 boxes'''
+'''This Module counts determines if there are
+adequate keys to open n-1 boxes'''
+
 
 def canUnlockAll(boxes):
     '''Method to return True or False if all boxes could be opened
@@ -10,20 +12,20 @@ def canUnlockAll(boxes):
     totalKeys = len(haveKeys)
 
     for key in haveKeys:
-        #print(key)-> 0
-        #print(len(haveKeys)) -> 1
+        # print(key)-> 0
+        # print(len(haveKeys)) -> 1
         # print(boxes[key])
         for keys in boxes[key]:
-            #print(keys) -> 1
+            # print(keys) -> 1
             if (keys not in haveKeys) and (keys < totalBoxes):
                 haveKeys.append(keys)
     if (len(haveKeys) == totalBoxes):
         return True
     else:
         return False
-                #print(haveKeys)->[0, 1, 2, 3, 4]
-            #print(index, keys) #-> 0 [1], 1 [2], 2 [3], 3 [4], 4 []
-        
+        # print(haveKeys)->[0, 1, 2, 3, 4]
+        # print(index, keys) #-> 0 [1], 1 [2], 2 [3], 3 [4], 4 []
+
     #     if (keys not in haveKeys) and (keys < boxTotal)
     #     if index == 0:
     #         unlockedBox[index + 1] = False
@@ -39,11 +41,11 @@ def canUnlockAll(boxes):
     #             unlockedBox[key] = True
 
         # print(key) -> (1,2,3,4,5)
-    #print(unlockedBox)#-->(dict_items([(1, False), (2, True), (3, True), (4, True), (5, True)]))
+    # print(unlockedBox)#-->(dict_items([(1, False), (2, True),
+    #                        (3, True), (4, True), (5, True)]))
     #     if (unlockedBox.keys() == True):
     #         unlockedBox[key] = False
     # print(unlockedBox)
-
 
     # for key in haveKeys:
     #     if (key < len(boxes)) and (lockBox[key] != True):
@@ -51,25 +53,20 @@ def canUnlockAll(boxes):
     #     for vals in lockBox.values():
     #         print (vals)
 
-
-        
     # print("Box = {}, haveKeys = {}".format(box, haveKeys))
     #     for keys in haveKeys:
     #         if keys < len(boxes):
     #         # print(box, haveKeys)
     #             openDict[box + 1] = True
-                
+
     #         else:
     #             openDict[box + 1] = False
-                
+
     #     print(openDict)
 
-    
     # # if False in openDict.values():
     # #     return (False)
     # # else:
     # #     return (True)
 
-
-        
-            # print("Box #{} contains:{}".format(box, box_key))
+        # print("Box #{} contains:{}".format(box, box_key))

4b26b8526389d123127e432a23caf8aa2b816373
brerickner <breannarickner@gmail.com>
2021-07-29 16:37:18 -0500

    adding progress for linkedlist

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * listing_t *insert_node - listint_t **head, int number
+ * @head: head of linked list
+ * @number: number to insert
+ * description: function in C that inserts a number into a sorted singly linked list
+ * Return: the address of the new node, or NULL if it failed
+ */
+listint_t *insert_node(listint_t **head, int number){
+    listint_t *newNode = NULL, *buffNode = NULL;
+
+	newNode = malloc(sizeof(listint_t));
+        if (!newNode)
+		return (NULL);
+        newNode->n = number;
+	if (!(*head))
+                *head = newNode;
+    
+        if ((*head)->next->n > number)
+		newNode->next = *head;
+	for (buffNode = *head; buffNode->next && buffNode->next->n < number; buffNode = buffNode->next);
+        
+        newNode->next = buffNode->next;
+        buffNode->next = newNode;
+        
+	return (newNode);
+}

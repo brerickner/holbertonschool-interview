9f978479d69e65947bb2f18c26d12c1126eb4027
brerickner <breannarickner@gmail.com>
2021-07-29 17:31:39 -0500

    adding

--- 0x01-insert_in_sorted_linked_list/0-main.c
+++ 0x01-insert_in_sorted_linked_list/0-main.c
@@ -4,7 +4,7 @@
 #include "lists.h"
 
 /**
- * main - create list and test insert in NULL / empty list
+ * main - create list and test insert multiple numbers
  *
  * Return: Always 0.
  */
@@ -13,11 +13,24 @@ int main(void)
 	listint_t *head;
 
 	head = NULL;
+	add_nodeint_end(&head, 0);
+	add_nodeint_end(&head, 1);
+	add_nodeint_end(&head, 2);
+	add_nodeint_end(&head, 3);
+	add_nodeint_end(&head, 4);
+	add_nodeint_end(&head, 98);
+	add_nodeint_end(&head, 402);
+	add_nodeint_end(&head, 1024);
 	print_listint(head);
 
 	printf("-----------------\n");
 
-	insert_node(&head, 972);
+	insert_node(&head, 5);
+	insert_node(&head, -32);
+	insert_node(&head, 5432);
+	insert_node(&head, 101);
+	insert_node(&head, 47);
+	insert_node(&head, 6405);
 
 	print_listint(head);
 

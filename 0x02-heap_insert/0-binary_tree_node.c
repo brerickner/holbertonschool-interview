4371a44770b0b7b691da50af172082ed21508288
brerickner <breannarickner@gmail.com>
2021-08-05 09:05:55 -0500

    adding after betty style improvements

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -1,28 +1,26 @@
 #include "binary_trees.h"
 
 /**
- * *binary_tree_node - binary_tree_t *parent, int value
- * @binary_tree_t: the binary three
- * @parent: parent node
- * @value: int
- * @tree: Pointer to the root of the tree to delete
- * description: function that creates a binary tree node
- * Return: pointer to the new node, or NULL on failure
- */
+* binary_tree_node - binary_tree_t *parent, int value
+* @parent: parent node
+* @value: int
+* description: function that creates a binary tree node
+* Return: pointer to the new node, or NULL on failure
+*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-        /* Declare memory */
-        binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
+	/* Declare memory */
+	binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
 
-        /* malloc check */
+	/* malloc check */
 	if (newNode == NULL)
 		return (NULL);
 
-        /* creates the new node with goodies */
+	/* creates the new node with goodies */
 	newNode->parent = parent;
 	newNode->n = value;
 	newNode->left = NULL;
 	newNode->right = NULL;
 
-        return (newNode);
+	return (newNode);
 }

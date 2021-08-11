508aba81354e1fb0a3aaafa5d292a81f789a47f8
brerickner <breannarickner@gmail.com>
2021-08-11 13:21:58 -0500

    renamed variable to fit reconfigured logic

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -9,7 +9,7 @@ def minOperations(n):
     needed to result in exactly n
     '''
 
-    ops = 2
+    chH = 2
     factList = []
 
     if (n <= 0):
@@ -18,16 +18,16 @@ def minOperations(n):
         return (0)
     if (n == 1):
         return (0)
-    while (ops * ops <= n):
-        if (n % ops):
-            ops += 1
+    while (chH * chH <= n):
+        if (n % chH):
+            chH += 1
         else:
             '''
             Floor division - truncates the decimal
             and returns the integer result
             '''
-            n //= ops
-            factList.append(ops)
+            n //= chH
+            factList.append(chH)
     if n > 1:
         factList.append(n)
     return (sum(factList))

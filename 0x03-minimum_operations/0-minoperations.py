85bb912908238c3c159cb551e26ba8eb14a75071
brerickner <breannarickner@gmail.com>
2021-08-11 13:10:18 -0500

    adding task output showing as correct

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,31 +1,32 @@
 #!/usr/bin/python3
 '''Miunimum Operations'''
+import sys
+
 
 def minOperations(n):
-    '''Method that calculates the fewest number of operations
-    needed to result in exactly n'''
+    '''
+    Method that calculates the fewest number of operations
+    needed to result in exactly n
+    '''
+
+    ops = 2
+    factList = []
+
     if (n <= 0):
-        return 0
-    if (type(n) == "int"):
-        print ("meow")
-        return 0
-    if (n == 1):
-        return 0
-    
-    # copy starts at 2
-    copies = 2
-    ops = 0
-    
-    while (ops <= n):
-        if (n % copies == 0):
-            copies += 1
+        return (0)
+    if (isinstance(n, "int")):
+        print("meow")
+        return (0)
+    while (ops * ops <= n):
+        if (n % ops):
             ops += 1
         else:
-            n /= copies
-            ops += copies
-    return (ops)
-
-
-
-
-            
+            '''
+            Floor division - truncates the decimal
+            and returns the integer result
+            '''
+            n //= ops
+            factList.append(ops)
+    if n > 1:
+        factList.append(n)
+    return (sum(factList))

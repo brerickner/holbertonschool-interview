7ecbcaa2d36bfb8365c8c2be0d9dfaa80f08acb2
brerickner <breannarickner@gmail.com>
2021-08-16 13:51:45 -0500

    setting up files

---
+++ 0x04-sandpiles/0-sandpiles.c
@@ -0,0 +1,14 @@
+#include "sandpiles.h"
+
+/**
+ * sandpiles_sum - int grid1[3][3], int grid2[3][3]
+ * @grid1: first 3x3 grid
+ * @grid2: second 3x3 grid
+ * description: function that computes the sum of two sandpiles
+ * Return: void
+ */
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
+
+}

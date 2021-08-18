2882991f92fd9f0fb3182338fbcf8c14b3571cc9
brerickner <breannarickner@gmail.com>
2021-08-18 09:32:35 -0500

    adding progress. Printing new grid but has an equal sign being printed

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -1,3 +1,4 @@
+
 #include "sandpiles.h"
 
 /**
@@ -10,7 +11,22 @@
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-    (void) grid1;
-    (void) grid2;
-    return;
+	int x, y, sumGrid[3][3];
+	int i, j;
+	/*Get the sums of two grids and put in new grid*/
+	for (x = 0; x < 3; x++)
+		for (y = 0; y < 3; y++)
+			sumGrid[x][y] = grid1[x][y] + grid2[x][y];
+
+	/*printing the sum grid*/
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (j)
+				printf(" ");
+			printf("%d", sumGrid[i][j]);
+		}
+		printf("\n");
+	}
 }

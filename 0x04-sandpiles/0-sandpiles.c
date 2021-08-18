619e83b7ed32474560777067d4f8e0580e4bdcd1
brerickner <breannarickner@gmail.com>
2021-08-18 14:56:09 -0500

    adding progress. about to start toppling

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -12,10 +12,33 @@
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
 	int x, y;
+	int sumPile[3][3];
 	/*int i, j, sumGrid[3][3];*/
 
 	/*Get the sums of two grids and put in new grid*/
 	for (x = 0; x < 3; x++)
+	{
 		for (y = 0; y < 3; y++)
-			grid1[x][y] += grid2[x][y];
+		{
+			sumPile[x][y] = grid1[x][y] + grid2[x][y];
+		}
+	}
+	printf("=\n");
+	print_grid(sumPile);
+	printf("***DEBUG nextpile and grid1 should be equal**\n");
+}
+static void print_grid(int grid[3][3])
+{
+	int i, j;
+
+	for (i = 0; i < N; i++)
+	{
+		for (j = 0; j < N; j++)
+		{
+			if (j)
+				printf(" ");
+			printf("%d", grid[i][j]);
+		}
+		printf("\n");
+	}
 }

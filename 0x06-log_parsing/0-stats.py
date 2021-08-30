c3f8f25bcdf17a446a55b215548bca8387c3a92d
brerickner <breannarickner@gmail.com>
2021-08-30 16:07:36 -0500

    adding progress

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -5,8 +5,12 @@ import os
 import sys
 import fileinput
 
+thisList = []
+statusDict = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+file_size = 0
+
 try:
-    for line in fileinput.input():
-        print("Meow {}", format(line))
-except KeyboardInterrupt:
-    sys.exit()
+    for index, line in enumerate(fileinput.input()):
+        file_size += int(line.split(" ")[-1])
+except KeyboardInterrupt: 
+    print("File size: {}".format(file_size))

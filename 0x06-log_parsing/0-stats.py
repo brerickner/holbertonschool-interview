1f11c782712143247130ca46f405755c43010c56
brerickner <breannarickner@gmail.com>
2021-09-01 12:05:21 -0500

    adding progress for 0 stat. first loop seems wonky

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -18,19 +18,26 @@ if __name__ == "__main__":
         500: 0}
     file_size = 0
 
+    def printStuff():
+        '''Method to print when divisible by 10 or EOF/Ctr + C'''
+        print("File size: {}".format(file_size))
+        for key, val in sorted(statusDict.items()):
+            if (val):
+                print("{}: {}".format(key, val))
+
     try:
         for index, line in enumerate(fileinput.input()):
             status = int(line.split(" ")[-2])
-            if (index == 0 or index % 10 == 0):
-                file_size += int(line.split(" ")[-1])
-                print("File size: {}".format(file_size))
+
             if status in statusDict:
                 statusDict[status] += 1
-                print("{}: {}".format(status, statusDict[status]))
+            if (index % 10 == 0):
+                file_size += int(line.split(" ")[-1])
+                printStuff()
 
     except KeyboardInterrupt:
         print("File size: {}".format(file_size))
-    except Exception:
-        pass
+    # except Exception:
+    #     pass
     finally:
-        pass
+        printStuff()

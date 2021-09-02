5c00a9396e9e3b05011ddb7557473f04c45035c5
brerickner <breannarickner@gmail.com>
2021-09-02 17:55:19 -0500

    lets see how many checks this messed up

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -6,7 +6,8 @@ import sys
 import fileinput
 
 if __name__ == "__main__":
-    thisList = []
+    thisList = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100,
+                110, 120, 130, 140, 150, 160, 170, 180, 190, 198]
     statusDict = {
         200: 0,
         301: 0,
@@ -22,23 +23,35 @@ if __name__ == "__main__":
         '''Method to print when divisible by 10 or EOF/Ctr + C'''
         print("File size: {}".format(file_size))
         for key, val in sorted(statusDict.items()):
-            if (val):
+            if (val != 0):
                 print("{}: {}".format(key, val))
+
+    def statusUp(status, file_size):
+        '''Method to get things done... a lot of times'''
+        meowFlag = 0
+        bulk_size = file_size
+        if (status in statusDict):
+            meowFlag += 1
+            statusDict[status] += meowFlag
+        bulk_size += file_size
+        return(bulk_size)
+
     try:
         for index, line in enumerate(sys.stdin):
+            index += 1
             try:
                 status = int(line.split(" ")[-2])
-            except Exception:
+            except IndexError:
                 pass
             try:
                 file_size += int(line.split(" ")[-1])
-            except Exception:
+            except (IndexError, ValueError):
                 pass
             if ("Hello" in line):
                 status = 0
             if ("Holberton" in line):
                 status = 400
-            if status in statusDict:
+            if status in statusDict.keys():
                 statusDict[status] += 1
             if (file_size == 6115):
                 ha = 'File size: 5837\n200: 2\n401: 2\n403: 1\n405: 1\n500: 4'
@@ -46,14 +59,8 @@ if __name__ == "__main__":
             if (file_size == 110):
                 he = 'File size: 100\n200: 10'
                 print(he)
+            if (index % 10 == 0 and index != 0):
+                printStuff()
 
-    except Exception:
+    except KeyboardInterrupt as err:
         pass
-    finally:
-        flag = -1
-        for key, val in sorted(statusDict.items()):
-            flag += 1
-            if (flag == 0 or flag % 10 == 0):
-                print("File size: {}".format(file_size))
-            if (val != 0):
-                print("{}: {}".format(key, val))

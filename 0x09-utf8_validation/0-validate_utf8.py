7bc6306fd1eeb694118f3b89a6efa51e6c3fc65b
brerickner <breannarickner@gmail.com>
2021-09-23 18:06:08 -0500

    adding progress for task

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module to determine if data set represents a valid UTF-8 encoding"""
+
+
+def validUTF8(data):
+    '''Method that returns true if data is valid UTF-8'''
+    valid_utf8 = True
+    try:
+        str(bytes(data), 'utf-8')
+    except Exception:
+        valid_utf8 = False
+
+    return valid_utf8

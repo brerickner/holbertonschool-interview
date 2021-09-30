34b3b7f107dca90bffe14226d910a5dab6012925
brerickner <breannarickner@gmail.com>
2021-09-30 09:31:03 -0500

    about to switch tactics but it's compiling right now

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -1,5 +1,6 @@
 #include "slide_line.h"
 
+
 /**
 * slide_line - int *line, size_t size, int direction
 * @line: the line
@@ -9,42 +10,91 @@
 * Return: 1 upon success. Else 0
 */
 
+static void print_array(int const *array, size_t size)
+{
+    size_t i;
+
+    printf("BUFF ARRAY: ");
+    for (i = 0; i < size; i++)
+        printf("%s%d", i > 0 ? ", " : "", array[i]);
+    printf("\n");
+}
+
 int slide_line(int *line, size_t size, int direction)
 {
-	size_t i;
+	int index, jIndex = 0, zeros = 0, dups = 0;
+	int buffArray[size - zeros];
+	int current, lftNeighb, rtNeighb;
 
 	if (!line)
 	{
 		return (0);
 	}
-	/* If L */
-	if (direction == 0 || direction == 1)
-	{
-		for (i = 0; i < size; i++)
-		{
-			if (line[i + 1] && line[i + 1] == line[i])
-			{
-				line[i] = line[i] + line[i + 1];
-				if (line[i + 2])
-				{
-					line[i + 1] = line[i + 2];
-				}
-
-				else if (line[i + 1] && line[i + 1] == line[i])
-				{
-					line[i + 1] = slide_line(line, size, direction);
-
-					line[i + 1] = 0;
-				}
-			}
-		}
-		return (1);
-	}
-
-	else
+	if (direction != 0 && direction != 1)
 	{
 		return (0);
 	}
+	if (size < 1)
+	{
+		return (0);
+	}
+
+	if (direction == SLIDE_LEFT)
+	{
+		zeros = 0;
+		jIndex = 0;
+		for (index = 0; index < (int)size - 1; index++)
+		{
+			current = line[index];
+			lftNeighb = line[index + 1];
+			jIndex = 0;
+			while ((zeros + jIndex) < (int)size)
+			{	
+				if (current == 0)
+				{
+					zeros++;
+					continue;
+				}
+				else if (current != 0 && current == lftNeighb)
+				{
+					zeros++;
+					buffArray[jIndex] = current + lftNeighb;
+					print_array(buffArray, size);
+					jIndex++;
+					continue;
+				}
+			}
+		}
+		
+	}
+	if (direction == SLIDE_RIGHT)
+	{
+		zeros = 0;
+		jIndex = 0;
+		for (index = size - 1; index > -1; index--)
+		{
+			current = line[index];
+			rtNeighb = line[index - 1];
+			while ((zeros + jIndex) < (int)size)
+			{
+				if (current == 0)
+				{
+					zeros++;
+					continue;
+				}
+				if (current == rtNeighb)
+				{
+					zeros++;
+					buffArray[jIndex] = current + rtNeighb;
+					jIndex++;
+					continue;
+				}
+			}
+		}
+
+		
+	}
+	return (1);
 }
 
 

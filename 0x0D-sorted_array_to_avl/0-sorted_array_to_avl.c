32b2c2a2098484a391b22786be79a953b353b33f
brerickner <breannarickner@gmail.com>
2021-10-27 17:18:17 -0500

    reverting back to when there were checks

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -1,30 +1,24 @@
 #include "binary_trees.h"
-
-
 /**
- * avl_t - int *array, avl_t_t *parent, int value
+ * binary_tree_node - binary_tree_t *parent, int value
  * @parent: root node
  * @value: value contained in node
- * description: function that creates binary tree nodes recursively through AVL
+ * description: function that creates a binary tree node
  * Return: Pointer to new node. Else NULL
  */
 
-avl_t *binary_tree_node(int *array, avl_t *parent, int begin, int end)
+avl_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	avl_t *newNode = malloc(sizeof(avl_t));
-	int value;
 
 	if (newNode == NULL)
 		return (NULL);
-	value = (begin + end) / 2;
-	if (begin > end)
-		return (NULL);
-	/* create the new node with goodies */
-	newNode->parent = parent;
-	newNode->n = array[value];
-	newNode->left = binary_tree_node(array, newNode, begin, (value - 1));
-	newNode->right = binary_tree_node(array, newNode, (value + 1), end);
 
+/* create the new node with goodies */
+	newNode->parent = parent;
+	newNode->n = value;
+	newNode->left = NULL;
+	newNode->right = NULL;
 	return (newNode);
 }
 
@@ -41,5 +35,5 @@ avl_t *sorted_array_to_avl(int *array, size_t size)
 	if (array == NULL || size == 0)
 		return (NULL);
 	
-	return(binary_tree_node(array, NULL, 0, size - 1));
+	return(binary_tree_node(NULL, array[0]));
 }

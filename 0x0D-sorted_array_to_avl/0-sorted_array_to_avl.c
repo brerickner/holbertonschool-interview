d954cae1eabafbb1c021370d7bb10cd3145cc08b
brerickner <breannarickner@gmail.com>
2021-10-27 16:30:47 -0500

    adding progress. My output looks the same as theirs I don't get what's going on

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -1,24 +1,37 @@
 #include "binary_trees.h"
+
+void _binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+	_binary_tree_delete(tree->left);
+	_binary_tree_delete(tree->right);
+	free(tree);
+}
+
 /**
- * binary_tree_node - binary_tree_t *parent, int value
+ * avl_t - int *array, avl_t_t *parent, int value
  * @parent: root node
  * @value: value contained in node
- * description: function that creates a binary tree node
+ * description: function that creates binary tree nodes recursively through AVL
  * Return: Pointer to new node. Else NULL
  */
 
-avl_t *binary_tree_node(binary_tree_t *parent, int value)
+avl_t *binary_tree_node(int *array, avl_t *parent, int begin, int end)
 {
 	avl_t *newNode = malloc(sizeof(avl_t));
-
+	
 	if (newNode == NULL)
 		return (NULL);
-
-/* create the new node with goodies */
+	int value = (begin + end) / 2;
+	if (begin > end)
+		return (NULL);
+	/* create the new node with goodies */
 	newNode->parent = parent;
-	newNode->n = value;
-	newNode->left = NULL;
-	newNode->right = NULL;
+	newNode->n = array[value];
+	newNode->left = binary_tree_node(array, newNode, begin, (value - 1));
+	newNode->right = binary_tree_node(array, newNode, (value + 1), end);
+
 	return (newNode);
 }
 
@@ -35,5 +48,5 @@ avl_t *sorted_array_to_avl(int *array, size_t size)
 	if (array == NULL || size == 0)
 		return (NULL);
 	
-	return(binary_tree_node(NULL, array[0]));
+	return(binary_tree_node(array, NULL, 0, size - 1));
 }

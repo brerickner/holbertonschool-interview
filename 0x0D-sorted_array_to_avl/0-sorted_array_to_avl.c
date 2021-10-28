26f87bdf71694d4f73c07a0b3744c1be38ce83cf
brerickner <breannarickner@gmail.com>
2021-10-28 11:45:52 -0500

    reverting

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -1,57 +1,39 @@
 #include "binary_trees.h"
 /**
- * binary_tree_node - binary_tree_t *parent, int indexer
- * @array: the array
- * @size: the size
+ * binary_tree_node - binary_tree_t *parent, int value
  * @parent: root node
- * @indexer: indexer contained in node
+ * @value: value contained in node
  * description: function that creates a binary tree node
  * Return: Pointer to new node. Else NULL
  */
 
-avl_t *binary_tree_node(int *array, avl_t *parent, int indexer, int size)
+avl_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	avl_t *newNode = malloc(sizeof(avl_t));
 
 	if (newNode == NULL)
 		return (NULL);
 
-	if (indexer > size)
-	    free(newNode);
-		return (NULL);
-
 /* create the new node with goodies */
-	newNode->n = array[(indexer + size) / 2];
 	newNode->parent = parent;
-	newNode->left = (
-					binary_tree_node(
-						array,
-						newNode,
-						0,
-						(((indexer + size) / 2) - 1))
-					);
-	newNode->right = (
-					binary_tree_node(
-						array,
-						newNode,
-						(((indexer + size) / 2) + 1),
-						0)
-					);
+	newNode->n = value;
+	newNode->left = NULL;
+	newNode->right = NULL;
 	return (newNode);
 }
 
 /**
 * sorted_array_to_avl - int *array, size_t size
 * @array: pointer to first element of the array to be converted
-* @size: size_t indexer representing number of elements in the array
+* @size: size_t value representing number of elements in the array
 * description: program that creates an AVL tree from an array
 * that has no duplicate values. 3 functions max. Functions cannot rotate.
 * Return: pointer to root node of newly created AVL tree or NULL if it fails.
 */
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
-	if (array == NULL || size < 1)
+	if (array == NULL || size == 0)
 		return (NULL);
 
-	return (binary_tree_node(array, NULL, 0, size - 1));
+	return (binary_tree_node(NULL, array[0]));
 }

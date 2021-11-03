db6d92e5e507b027768019f70f8ae20b43d44fa2
brerickner <breannarickner@gmail.com>
2021-11-03 15:12:10 -0500

    Transfer comment sections over from sandbox with correct output to explain the process that finally worked

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -4,22 +4,23 @@
  * linear_skip - skiplist_t *list, int value
  * @list: Pointer to head of sorted ascending skip list
  * @value: Value to search for.
- * Description: Function that searches for a value in a sorted skip list
- * of integers. None of express lane placed every index which is a multiple
- * of the square root of the size of the list.
- * Print 'Value checked at index {} = {value}'
+ * Description: Function that searches for a value in a sorted
+ * ascending skip list of ints using an express lane.
  *
  * Return: Pointer to first node where 'value' is located, or NULL if value
- * not present in list or head is NULL.
+ * not present in list or if head is NULL.
  */
 
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
 	skiplist_t *normalLane, *expressLane = NULL;
 
+	/* WHEN NO LIST*/
 	if (list == NULL)
 		return (NULL);
 
+	/* Compare values in express lane to determine if value is present in*/
+	/*express Lane values*/
 	for (expressLane = list->express, normalLane = list; expressLane;
 	     normalLane = expressLane, expressLane = expressLane->express)
 	{
@@ -28,15 +29,18 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 
 		if (expressLane->express == NULL || expressLane->n >= value)
 		{
+			/* Reaches end of express nodes without finding value */
 			if (expressLane->express == NULL && expressLane->n < value)
 			{
+				/* Calculate for in between indexes */
 				for (normalLane = expressLane; expressLane->next;
 					expressLane = expressLane->next)
 					;
 			}
 			printf("Value found between indexes [%lu] and [%lu]\n",
-				 normalLane->index, expressLane->index);
+				normalLane->index, expressLane->index);
 
+			/* Iterate through normal lane checking for value */
 			for (; normalLane; normalLane = normalLane->next)
 			{
 				printf("Value checked at index [%lu] = [%d]\n",

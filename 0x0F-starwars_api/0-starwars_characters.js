430bf26126c8a456dd5649f49a1402302602e0ee
brerickner <breannarickner@gmail.com>
2021-11-11 08:34:33 -0600

    adding progress. Appears to print in correct order some of the time

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,2 +1,38 @@
 #!/usr/bin/node
 
+// Script that prints all characters of a Star Wars movie using the Star Wars API 'https://swapi.co/api/films/{movie_id}/'.
+// Print one character per line, same order as "characters" list in the /films/{movie_id}/ response.
+// Must use request module.
+// Using swapi-node wrapper package.
+
+const request = require('request');
+
+const filmId = process.argv[2];
+let url = 'https://swapi-api.hbtn.io/api/films/';
+
+url = url + filmId + '/';
+
+request(url, (error, response, body) => {
+  // Printing the error if occurred
+  if (error) console.log(error);
+
+  // Printing status code
+  // console.log(response.statusCode);
+
+  // Printing characters from body in JSON format
+  const Characters = JSON.parse(body).characters;
+
+  // Extract new url for each character to make a new request for their name
+  for (const people of Characters) {
+    request(people, (error, response, body) => {
+      // Printing the error if occurred
+      if (error) console.log(error);
+
+      // Printing status code
+      // console.log(response.statusCode);
+
+      // Printing character name
+      console.log(JSON.parse(body).name);
+    });
+  }
+});
--- 0x0F-starwars_api/README.md
+++ 0x0F-starwars_api/README.md
@@ -1,1 +1,4 @@
-0x0F-starwars_api
+##  0x0F-starwars_api
+
+##  Credits
+###  [![js-semistandard-style](https://img.shields.io/badge/code%20style-semistandard-brightgreen.svg)](https://github.com/standard/semistandard)a

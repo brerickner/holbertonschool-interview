20b323bc5e5c650d53727a2383424e6e8cd98662
brerickner <breannarickner@gmail.com>
2021-11-22 20:39:23 -0600

    saving progress. Output prints the end result correctly but needs work on printing array after swaps

---
+++ 0x11-heap_sort/0-heap_sort.c
@@ -0,0 +1,51 @@
+#include "sort.h"
+
+/**
+* heap_sort - int *array, size_t size
+* @array: The array to be printed
+* @size: Number of elements in @array
+* description: Function that sorts an array of ints in ascending order
+* using the sift-down heap sort algorithm printing array after each swap.
+* Return: void
+*/
+void heap_sort(int *array, size_t size)
+{
+	int i;
+	
+	for (i = (size - 2) / 2; i >= 0; i--) 
+	{
+		downheap(array, size, i);
+	}
+	for (i = 0; i < (int)size; i++)
+	{
+        	int t = array[size - i - 1];
+		array[size - i - 1] = array[0];
+		array[0] = t;
+		downheap(array, size - i - 1, 0);
+		print_array(array, size - i);
+	}
+}
+
+void downheap(int *array, size_t size, int i)
+{
+	while (1)
+	{
+		int j = max_heapify(array, size, i, 2 * i + 1, 2 * i + 2);
+		if (j == i)
+			break;
+		int t = array[i];
+		array[i] = array[j];
+		array[j] = t;
+		i = j;
+	}
+}
+
+int max_heapify(int *array, int size, int i, int j, int k)
+{
+	int m = i;
+	if (j<size && array[j] > array[m])
+		m = j;
+	if (k<size && array[k] > array[m])
+		m = k;
+	return m;
+}

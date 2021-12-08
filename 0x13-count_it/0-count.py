e39032ff6eb8a8bc2edaabe93942c00155e1c513
brerickner <breannarickner@gmail.com>
2021-12-08 14:15:15 -0600

    saving progress. Pep8 compliant and sorted output based on value starting with highest

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -9,6 +9,7 @@
         python3 count_it.py <subreddit> <keyword>
 '''
 
+from collections import OrderedDict
 import requests
 
 
@@ -17,7 +18,7 @@ def count_words(subreddit, word_list, after=None, word_dict={}, flag=0):
         Method that recursively queries reddit API for hot articles printing
         the number of times each given keyword is present.
     """
-
+    word_dict = OrderedDict(word_dict)
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
 
     ''' page through listings'''
@@ -29,7 +30,7 @@ def count_words(subreddit, word_list, after=None, word_dict={}, flag=0):
             headers=headers,
             params=parameter,
             allow_redirects=False)
-    except:
+    except BaseException:
         return(None)
 
     if response.status_code != 200:
@@ -39,7 +40,7 @@ def count_words(subreddit, word_list, after=None, word_dict={}, flag=0):
         meow = response.get('data')
         children = meow.get('children')
         after = meow.get('after')
-    except:
+    except BaseException:
         return(None)
 
     # print(meow.get('children')[0].get('data')['title'])
@@ -49,14 +50,24 @@ def count_words(subreddit, word_list, after=None, word_dict={}, flag=0):
         for words in title.split():
             for word in word_list:
                 if words == word.lower():
-                    if words in word_dict.keys():
+                    if word.lower() in word_dict.keys():
                         word_dict[word.lower()] += 1
                     else:
                         word_dict[word.lower()] = 1
 
-    if after is None:
-        for x in sorted(word_dict.keys(), reverse=True):
-            print("{}: {}".format(x, word_dict[x]))
-    else:
+    if after:
         return(count_words(subreddit, word_list, after, word_dict, flag))
-    return(None)
+    else:
+
+        '''
+            If you construct a dictionary with the words as keys and the number
+            of occurrences of each word as value, simplified here as:
+                d = defaultdict(int)
+                for w in text.split():
+                    d[w] += 1
+            then you can get a list of the words, ordered by frequency of use
+            with sorted(d, key=d.get) - the sort iterates over the dictionary
+            keys, using the number of word occurrences as a sort key
+        '''
+        for x in sorted(word_dict, key=word_dict.get, reverse=True):
+            print('{}: {}'.format(x, word_dict[x]))

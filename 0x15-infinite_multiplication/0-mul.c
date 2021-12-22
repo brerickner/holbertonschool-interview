51e1dc9de6a23ec0a24bbb133d5ae6527034546f
brerickner <breannarickner@gmail.com>
2021-12-22 12:04:26 -0600

    fixing those illegat C++ comments

--- 0x15-infinite_multiplication/0-mul.c
+++ 0x15-infinite_multiplication/0-mul.c
@@ -15,12 +15,12 @@ int main(int argc, char *argv[])
 		int c = a * b;
 		printf("%d\n", c);
 	}
-	// int a,b,result;
-	// a = strlen("87879");
-	// b = strlen("406749840980484980487487987");
-	// result = strlen("1654454331376");
-	// printf("a: %d\n",a);
-	// printf("b: %d\n",b);
-	// printf("result: %d\n",result);
-	// printf ("Error");
+	/* int a,b,result;*/
+	/* a = strlen("87879");*/
+	/* b = strlen("406749840980484980487487987");*/
+	/* result = strlen("1654454331376");*/
+	/* printf("a: %d\n",a);*/
+	/* printf("b: %d\n",b);*/
+	/* printf("result: %d\n",result);*/
+	/* printf ("Error");*/
 }

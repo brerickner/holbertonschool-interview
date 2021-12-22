8d05134bb1167270c75116b1eb9e839c862dc88a
brerickner <breannarickner@gmail.com>
2021-12-22 11:56:47 -0600

    trying to get explicit!

--- 0x15-infinite_multiplication/0-mul.c
+++ 0x15-infinite_multiplication/0-mul.c
@@ -4,8 +4,8 @@ int main()
 {
 	int a,b,result;
 	a = strlen("87879");
-	b = strlen("406749840980484980487487987");"
-	result = strlen("1654454331376");"
+	b = strlen("406749840980484980487487987");
+	result = strlen("1654454331376");
 	printf("a: %d\n",a);
 	printf("b: %d\n",b);
 	printf("result: %d\n",result);

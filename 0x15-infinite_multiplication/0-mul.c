3c063bdcf0b71400b34a61ce354cb22edd87a483
brerickner <breannarickner@gmail.com>
2021-12-22 12:02:59 -0600

    coverin errors and playin around with atoi

--- 0x15-infinite_multiplication/0-mul.c
+++ 0x15-infinite_multiplication/0-mul.c
@@ -1,13 +1,26 @@
 #include "holberton.h"
 
-int main()
+int main(int argc, char *argv[])
 {
-	int a,b,result;
-	a = strlen("87879");
-	b = strlen("406749840980484980487487987");
-	result = strlen("1654454331376");
-	printf("a: %d\n",a);
-	printf("b: %d\n",b);
-	printf("result: %d\n",result);
-	return (0);
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	else
+	{
+		int a = atoi(argv[1]);
+		int b = atoi(argv[2]);
+		int c = a * b;
+		printf("%d\n", c);
+	}
+	// int a,b,result;
+	// a = strlen("87879");
+	// b = strlen("406749840980484980487487987");
+	// result = strlen("1654454331376");
+	// printf("a: %d\n",a);
+	// printf("b: %d\n",b);
+	// printf("result: %d\n",result);
+	// printf ("Error");
 }

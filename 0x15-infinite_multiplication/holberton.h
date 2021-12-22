005b42d327de4437c16354d96d9579b6fbe96868
brerickner <breannarickner@gmail.com>
2021-12-22 11:53:47 -0600

    saving file goodies

---
+++ 0x15-infinite_multiplication/holberton.h
@@ -0,0 +1,11 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int _putchar(char c);
+
+#endif /*_HOLBERTON_H_*/

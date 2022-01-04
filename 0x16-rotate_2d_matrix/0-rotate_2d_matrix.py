0d332099df4d39ad6a187f2dddead6a70326773b
brerickner <breannarickner@gmail.com>
2022-01-04 11:14:35 -0600

    saving progress. Pep8 compliant and output matches example given

---
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module that Rotate 2D Matrix 90 degrees clockwise
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """
+    Rotates 2D matrix in-place 90 degrees to the right.
+    Assume matrix has 2 dimensions and not empty.
+    Return: void
+    """
+    meow = [[]]
+    meow = list(zip((*matrix[::-1])))
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            matrix[i][j] = meow[i][j]

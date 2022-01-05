f58a0a535d8d0f3e5bb4e8d24b7ea3de33eaa0cc
brerickner <breannarickner@gmail.com>
2022-01-05 10:59:25 -0600

    saving again

--- 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -6,7 +6,7 @@ def rotate_2d_matrix(matrix):
     """ Function rotate_2d_matrix -> takes 2d matrix as parameter. """
 
     meow = [[]]
-    meow = list(zip((*matrix[::-1])))
+    meow = list(zip(*matrix[::-1]))
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             matrix[i][j] = meow[i][j]

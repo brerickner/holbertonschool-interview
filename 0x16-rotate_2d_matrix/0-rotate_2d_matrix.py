c19ccbeb91d16c91b64f23512f427829ea3d7da7
brerickner <breannarickner@gmail.com>
2022-01-05 10:42:10 -0600

    adding spaces for..... clarity

--- 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Rotate_2d_matrix"""
+""" Rotate_2d_matrix """
 
 
 def rotate_2d_matrix(matrix):
-    """Function rotate_2d_matrix -> takes 2d matrix as parameter."""
+    """ Function rotate_2d_matrix -> takes 2d matrix as parameter. """
 
     meow = [[]]
     meow = list(zip((*matrix[::-1])))

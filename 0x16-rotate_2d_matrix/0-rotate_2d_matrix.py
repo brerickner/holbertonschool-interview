7bfdfde242c56cba99c91f4ea1ec5ce4affe759d
brerickner <breannarickner@gmail.com>
2022-01-05 10:34:29 -0600

    Redid documentation to be better

--- 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""Module rotate_2d_matrix"""
+"""Rotate_2d_matrix"""
 
 
 def rotate_2d_matrix(matrix):
-    """Function rotate_2d_matrix -> takes positive int 'walls' as parameter
-    @matrix:
-        2d matrix of integers
-    Description:
-        Rotates 2d matrix 90 degrees clockwise.
-    Returns:
-        Nothing. Rotate in place."""
+    """Function rotate_2d_matrix -> takes 2d matrix as parameter."""
+    """
+        @matrix:
+            2d matrix of integers
+        Description:
+            Rotates 2d matrix 90 degrees clockwise.
+        Returns:
+            Nothing. Rotate in place.
+    """
 
     meow = [[]]
     meow = list(zip((*matrix[::-1])))
